578a1e381a39d52b3e07360bcdf997acb934c982
Robert Deaton <rdeaton@platipy.org>
2017-12-20 13:33:09 -0800

    Start configuring i3

--- generic/bootstrap.sh
+++ generic/bootstrap.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Things we assume have been done before now:
+# git is installed (sudo apt install git)
+# ssh key is generated (ssh-keygen -t rsa -b 4096)
+
 echo "Allow passwordless sudo for this script"
 sudo echo "Thanks"
 
@@ -7,8 +11,14 @@ sudo apt update
 sudo apt install -y stow
 sudo apt install -y git
 
+
 cd ../dotfiles
 stow -t ~ X11
 cd -
 
 
+sudo apt install i3
+
+cd ../dotfiles
+stow -t ~ i3
+cd -
