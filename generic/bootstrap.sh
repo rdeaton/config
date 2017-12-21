34748874bf26ac127306f157975e3af7717330b6
Robert Deaton <rdeaton@platipy.org>
2017-12-21 07:11:56 -0800

    Startup compton and redshift, install Chrome

--- generic/bootstrap.sh
+++ generic/bootstrap.sh
@@ -7,6 +7,7 @@
 echo "Allow passwordless sudo for this script"
 sudo echo "Thanks"
 
+sudo apt-add-repository universe
 sudo apt update
 sudo apt install -y stow
 sudo apt install -y git
@@ -22,3 +23,15 @@ sudo apt install i3
 cd ../dotfiles
 stow -t ~ i3
 cd -
+
+# Google Chrome
+cd /tmp
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+cd -
+sudo apt -f install
+
+
+# Nicer graphics
+sudo apt install compton
+sudo apt install redshift
