014f6a26b76bd78b2165cf76b2cafc7c814435cb
Robert Deaton <rdeaton@platipy.org>
2019-03-13 13:54:47 -0700

    Add unclutter support

--- generic/bootstrap.sh
+++ generic/bootstrap.sh
@@ -18,6 +18,7 @@ sudo apt install -y redshift
 sudo apt install -y build-essential
 sudo apt install -y python3-pip
 sudo apt install -y autokey-gtk
+sudo apt install -y unclutter
 
 mkdir -p .build
 
