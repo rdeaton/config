ddf7a1b02ded937cbed9cd510ccd58af008e4a7a
Robert Deaton <rdeaton@platipy.org>
2019-03-14 11:35:55 -0700

    clipster and roficlip

--- setup/rofi.sh
+++ setup/rofi.sh
@@ -4,7 +4,7 @@ sudo apt install -y rofi
 sudo apt install -y rofi-dev qalc libqalculate-dev
 pushd .build
 git clone https://github.com/svenstaro/rofi-calc.git
-pushd rofi-calc
+cd rofi-calc
 git pull
 autoreconf -i
 mkdir build
@@ -12,4 +12,20 @@ cd build
 ../configure
 make
 sudo make install
+popd
+
+# clipster and rofi integration
+sudo apt install -y python-gi gir1.2-gtk-3.0
+pushd .build
+git clone https://github.com/mrichar1/clipster.git
+cd clipster
+git pull
+cp clipster ~/.local/bin/clipster
+popd
+
+pushd .build
+git clone https://github.com/gilbertw1/roficlip.git
+cd roficlip
+git pull
+cp roficlip ~/.local/bin/roficlip
 popd
