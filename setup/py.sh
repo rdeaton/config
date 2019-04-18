6e5ab3fe620d7e2afd32262a28f1fa090ec2ca03
Robert Deaton <rdeaton@platipy.org>
2019-04-17 20:55:17 -0700

    More python dependencies

--- setup/py.sh
+++ setup/py.sh
@@ -1,3 +1,4 @@
+sudo apt install python3-venv
 # https://github.com/pyenv/pyenv/wiki/Common-build-problems
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
 # Poetry
