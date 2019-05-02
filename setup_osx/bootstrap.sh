5c0d7dddc1f9892655bb6f241da2a265ff44fd8e
Robert Deaton <rdeaton@platipy.org>
2019-05-02 13:12:58 -0700

    OS X bootstrap

---
+++ setup_osx/bootstrap.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -ex
+
+echo "Allow passwordless sudo for this script"
+sudo echo "Thanks"
+
+# Get python3 and pip installed for pip based utils
+brew install python
+
+# dotfiles maintenance
+brew install stow
+mkdir -p ~/.local/bin
+
+# Terminal Environment
+brew install neovim
+brew install fzf
+brew install rg
+brew install tmux
+pip3 install tmuxp
+cd ~/.local/bin
+curl -O https://s3-eu-west-1.amazonaws.com/record-query/record-query/x86_64-apple-darwin/rq
+chmod +x rq
+cd -
+
+mkdir -p ~/.config/nvim/bundle/
+if [[ ! -d ~/.config/nvim/bundle/Vundle.vim ]]; then
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
+fi
+mkdir -p ~/.tmux/plugins
+if [[ ! -d ~/.tmux/plugins/tpm ]]; then
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+brew install --HEAD universal-ctags/universal-ctags/universal-ctags
+brew tap homebrew/cask-fonts
+brew cask install font-inconsolata-for-powerline
+
+# GUI Environment
+brew cask install flycut
+brew cask install google-chrome
+brew cask install flux
+brew cask install vlc
+
+# Python tooling
+if [[ ! -d "~/.pyenv" ]]; then
+	curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+	# Set pyenv up to work in this shell, future runs will be covered by dotfiles
+	export PATH="~/.pyenv/bin:$PATH"
+	eval "$(pyenv init)"
+	eval "$(pyenv virtualenv-init)"
+fi
+
+
+# Misc/Uncategorized
+brew install youtube-dl
+brew install streamlink 
+
+# Disable spotlight, it's a performance killer
+sudo mdutil -a -i off
